847571e4395a10fdfd8e90085c5ae1af7feca63e
Zeljan <zeljan.zivkov@etondigital.com>
2025-01-14 14:54:11 +0100

    Edit single lead details

--- cypress/e2e/ Admin Panel/editSingleLeadDetails.cy.js
+++ cypress/e2e/ Admin Panel/editSingleLeadDetails.cy.js
@@ -28,7 +28,7 @@ describe('Verify Editing Lead Details', () => {
                 .contains('Lead details')
                 .click({ force: true });
         //fali mi url ovde koj ide posle klika na lead details
-        // ?cy.url().should('include', 'app/admin-panel?page=1&itemsPerPage=5'); /
+        // ?cy.url().should('include', 'app/admin-panel?page=1&itemsPerPage=5'); 
     });
 
     it('should edit basic info successfully', () => {
@@ -98,42 +98,31 @@ describe('Verify Editing Lead Details', () => {
         cy.get('.undefined') 
                 .find('span') 
                 .contains('Edit method')
-                .click({ force: true });
-
+                .click({ force: true }).wait(2000);
+        
         cy.get('h5').contains('Communication method'); 
 
         cy.get('input[name="preferredCommunication"][value="Email"]')
-                .should('exist') 
-                .should('be.visible') 
-                .click(); 
-
-        cy.get('input[name="preferredCommunication"][value="Phone"]')
-                .should('exist') 
-                .should('be.visible') 
-                .click();       
-        cy.get('button[type="submit"]').contains('Save').click();
-
-        // ?cy.get('input[name="preferredCommunication"][value="Email"]').then(($email) => {
-        //     if ($email.is(':checked')) {
-        //         // Ako je Email selektovan, izaberi Phone
-        //         cy.get('input[name="preferredCommunication"][value="Phone"]')
-        //             .should('exist')
-        //             .should('be.visible')
-        //             .click();
-        //     } else {
-        //         // Ako Email NIJE selektovan, izaberi Email
-        //         cy.get('input[name="preferredCommunication"][value="Email"]')
-        //             .should('exist')
-        //             .should('be.visible')
-        //             .click();
-        //     }
-        // });
-        
-        // // Klikni na "Save" dugme nakon izbora
-        // ?cy.get('button[type="submit"]').contains('Save').click()
+            .should('exist')
+            .then(($email) => {
+             if ($email.attr('checked') !== undefined) {
+                // Ako je Email selektovan, izaberi Phone
+                cy.get('input[name="preferredCommunication"][value="Phone"]')
+                    .should('exist')
+                    .should('be.visible')
+                    .click();
+            } else {
+                // Ako Email NIJE selektovan, izaberi Email
+                cy.wrap($email)
+                    .should('exist')
+                    .should('be.visible')
+                    .click();
+                }
+            });
+        cy.get('button[type="submit"]').contains('Save').click()
     });
 
-    it.only('should edit Best contact time info successfully', () => {
+    it('should edit Best contact time info successfully', () => {
         cy.get('.undefined') 
                 .find('span') 
                 .contains('Edit time')
@@ -154,14 +143,17 @@ describe('Verify Editing Lead Details', () => {
                     cy.get('#time').contains(daysOfweek).should('be.visible');
                 });
 
-            // const preferedTime = [
-            //     'Morning (7AM - 10AM)',
-            //     'Early afternoon (11AM - 2PM)',
-            //     'Late afternoon (3PM - 6PM)'
-            //];
-            //     preferedTime.forEach(preferedTime => {
-            //         cy.get('#time').eq(1).find('name="preferredTime').contains(preferedTime);
-            //     });
+                const preferedTime = [
+                    'Morning (7AM - 10AM)',
+                    'Early afternoon (11AM - 2PM)',
+                    'Late afternoon (3PM - 6PM)'
+                ];
+                  
+                cy.get('input[name="preferredTime"]').each(($el, index) => {
+                    cy.wrap($el).should('have.value', preferedTime[index]);
+                });
+
+
         cy.get('input[name="preferredDays"][value="Mon"]')
             .should('exist') 
             .should('be.visible') 
@@ -174,10 +166,6 @@ describe('Verify Editing Lead Details', () => {
 
         cy.get('button[type="submit"]').contains('Save').click();
 
-        // cy.get('input[name="contactTime"]').should('be.visible'); 
-        // cy.get('input[name="contactTime"]').clear().type('Monday 9am-11am');
-        // cy.get('.lead-communication-method').should('contain', 'Email');
-        // cy.get('.lead-contact-time').should('contain', 'Monday 9am-11am');
     });
 
 });
